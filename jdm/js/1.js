82d644623dbff76dc76f6f8601e9a90c83f3bfde
vagmr <2737165206.qqcom>
2023-06-25 20:55:48 +0800

    new

--- jdm/js/1.js
+++ jdm/js/1.js
@@ -11,4 +11,23 @@ window.addEventListener("DOMContentLoaded", function () {
         location.assign('https://about.vagmrgpt.top');
     })
     // 顶部区域end
+    // 倒计时
+    var hour = document.querySelector('.h');
+    var minute = document.querySelector('.m');
+    var second = document.querySelector('.s');
+    var inputTime = +new Date('2023-6-30 18:00:00');
+    function Countdown() {
+        var nowTime = +new Date();//返回当前时间的毫秒数
+        var countTime = (inputTime - nowTime) / 1000;//返回剩余时间的秒数
+        var h = parseInt(countTime / 60 / 60 % 24);//通过取模找出不足一天的
+        h = h < 10 ? '0' + h : h;
+        hour.innerHTML = h;
+        var m = parseInt(countTime / 60 % 60);//分
+        m = m < 10 ? '0' + m : m;
+        minute.innerHTML = m;
+        var s = parseInt(countTime % 60);
+        s = s < 10 ? '0' + s : s;
+        second.innerHTML = s;
+    }
+    setInterval(Countdown, 1000);
 })
