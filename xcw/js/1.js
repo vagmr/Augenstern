0005887edb7a953bd524a62c1eca976f5afd62b9
vagmr <2737165206.qqcom>
2023-07-06 11:55:20 +0800

    add

--- xcw/js/1.js
+++ xcw/js/1.js
@@ -14,8 +14,10 @@ window.onload = function () {
             }
         }, 30);
     }
+    var focus = document.querySelector('.focus');
     var ul = document.querySelector(".ful");
     var li = document.querySelectorAll('li');
+    var w = focus.offsetWidth;
     console.log(li);
     var ol = document.querySelector('ol');
     for (var i = 0; i < li.length; i++) {
@@ -23,4 +25,15 @@ window.onload = function () {
         oli.setAttribute("data-index", i);
         ol.appendChild(oli);
     }
+    var index = 0;
+    var timer = setInterval(function () {
+        index++;
+        var translateX = - w * index;
+        ul.style.transition = "all 0.5s";
+        ul.style.transform = 'translateX(' + translateX + "px" + ')';
+        if (index == 3) {
+            index = -2;
+        }
+    }, 2000)
+    ol.children[0].className = "current";
 }
